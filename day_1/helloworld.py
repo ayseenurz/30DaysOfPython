8e119af8b9ddd90267ba1449d1602be0abad1524
Ayşenur Öz <aysenuroz00@icloud.com>
2026-09-22 22:47:51 +0300

    initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,28 @@
+from math import sqrt
+import math
+
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Pyhton', 'Finland']))
+print(type("Ayşenur"))
+print(type("ÖZ"))
+print(type("Türkiye"))
+
+a = 2
+b = 3
+x = 10 
+y = 8
+
+res = math.sqrt((a - x)**2 + (b - y)**2)
+print(res)
