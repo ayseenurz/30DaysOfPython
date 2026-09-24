4aa1c80d47a3273c91cd80c8fa422e19323368d0
Ayşenur Öz <aysenuroz00@icloud.com>
2026-09-25 00:01:45 +0300

    day_3 challange(operators) done

---
+++ day_3/operators.py
@@ -0,0 +1,101 @@
+#import math
+
+#my_age = 22
+#my_height = 1.67
+#some_complex_number = 3 + 4j
+
+#print("Enter base: ")
+#base = int(input())
+#print("Enter height: ")
+#height = int(input())
+#print("The area of a triangle is ", (base * height) / 2)
+
+#print("Enter side a value: ")
+#a = int(input())
+#print("Enter side b value: ")
+#b = int(input())
+#print("Enter side c value: ")
+#c = int(input())
+#print("Perimeter of a triangle is ", a + b + c)
+
+#print("Enter length: ")
+#length = int(input())
+#print("Enter width: ")
+#width = int(input())
+#print("Area of a rectangle is ", length * width)
+#print("Perimeter of a rectangle is ", (length + width) * 2)
+
+#print("Enter a radius: ")
+#radius = int(input())
+#print("Area of a circle is ", 3.14 * (radius ** 2))
+#print("Circumference of a circle is ", 2 * 3.14 * radius)
+
+#slope = 2 
+#print(slope)
+#x_intercept = 1
+#print(x_intercept)
+#y_intercept = -2
+#print(y_intercept)
+
+#x1 = 2 
+#print(x1)
+#x2 = 6
+#print(x2)
+#y1 = 2 
+#print(y1)
+#y2 = 10
+#print(y2)
+#slope_2 = (y2 - y1) / (x2 - x1)
+#print(slope_2)
+#euclidean_distance = math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
+#print(euclidean_distance)
+
+#print("Slope1 == Slope2 : ", slope == slope_2)
+
+#print("Enter a x value: ")
+#x_value = int(input())
+
+#y_value = ((x_value ** 2) + (6 * x_value) + 9)
+#if y_value == 0 :
+#    print("X value that makes Y 0 is ", x_value)
+#else:
+#    print(x_value, "is not a value we're searching for")
+
+#print('len("python") == len("dragon")',len("python") == len("dragon"))
+
+#print(("on" in "python") == ("on" in "dragon"))
+
+#sentence = "I hope that course is not full of jargon"
+#print("jargon" in sentence)
+
+#print(float(len("python")))
+#print(str(len("python")))
+
+#print("Enter a number to check if it's even or odd: ")
+#number = int(input())
+
+#if number % 2 == 0 : 
+#    print("The number", number, "is even")
+#else :
+#    print("The number",number,"is odd")
+
+#some_float_value = 2.7
+#print( 7 // 3 == int(some_float_value))
+
+#print(type('10') is type(10))
+#print(int(float('9.8')) == 10)
+
+#print("Enter hours: ")
+#hours = int(input())
+#print("Enter rate per hour: ")
+#rate = int(input())
+#print("Your weekly earning is", hours * rate)
+
+#print("How many years have you lived: ")
+#years = int(input())
+
+#print("You have lived for", years * 365 * 24 * 60 * 60,"seconds")
+
+#i = 0
+#for i in range(5):
+#    print(i+1,(i+1) ** 0, (i+1) ** 1, (i+1) ** 2, (i+1) ** 3)
